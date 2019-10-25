bd8316b60e49d74bdd396ce0b191d4ad110b138d
Anjali11 <anjali.parikh@mail.utoronto.ca>
2019-10-25 14:35:48 -0400

    Added You lost and try again screens.

--- phase1/app/src/main/java/com/example/gameboi/RockPaperScissors.java
+++ phase1/app/src/main/java/com/example/gameboi/RockPaperScissors.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.SortedSet;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -28,20 +33,52 @@ public class RockPaperScissors extends AppCompatActivity {
 
     }
 
-    public void RpSGamePlayed(View view) {
+    public void RpSGamePlayed(String playerValue) {
+
+        // Creating a map to store winning combination of moves
         HashMap winnersRpS = new HashMap();
         winnersRpS.put("Rock", "Scissors");
         winnersRpS.put("Scissors", "Paper");
         winnersRpS.put("Paper", "Rock");
 
-//        String ourValue = winnersRpS.keySet().random();
 
+        // Generating a random value from keys to be the computers move
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(2);
+        List keys = new ArrayList(winnersRpS.keySet());
+        Object ourValue = keys.get(randomInt);
+        System.out.println(ourValue);
 
-        Intent intent = new Intent(this, RpSWonRoundDisplay.class);
+        if (winnersRpS.get(playerValue) == ourValue) {
+            Intent intent = new Intent(this, RpSWonRoundDisplay.class);
 //        String message = "You have won this round!";
 //        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+            startActivity(intent);
+        } else if(winnersRpS.get(ourValue) == playerValue) {
+            // create new intent that will display you lost this round screen
+            Intent intent = new Intent(this, RpsLostRoundDisplay.class);
+            startActivity(intent);
+        } else {
+            // new intent that displays try again
+            Intent intent = new Intent(this, tryAgainDisplay.class);
+            startActivity(intent);
+        }
 
+    }
+
+    public void Rock(View view) {
+        RpSGamePlayed("Rock");
+
+    }
+
+    public void Paper(View view) {
+        RpSGamePlayed("Paper");
+
+    }
+
+    public void Scissors(View view) {
+        RpSGamePlayed("Scissors");
 
     }
 }
+
