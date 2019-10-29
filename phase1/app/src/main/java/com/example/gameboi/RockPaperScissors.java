eb9618a183578f335c924c9d6e32dd0e1d6fff53
sarrahmerchant <sarrah.merchant@mail.utoronto.ca>
2019-10-29 16:25:44 -0400

    Added views to leaderboard layout, fixed error in RPS class

--- phase1/app/src/main/java/com/example/gameboi/RockPaperScissors.java
+++ phase1/app/src/main/java/com/example/gameboi/RockPaperScissors.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Random;
