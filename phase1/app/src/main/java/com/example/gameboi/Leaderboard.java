975515ba8882b9e91f19546334dff3cee75481b2
Anjali11 <anjali.parikh@mail.utoronto.ca>
2019-10-30 14:20:18 -0400

    Fixed leaderboard layout

--- phase1/app/src/main/java/com/example/gameboi/Leaderboard.java
+++ phase1/app/src/main/java/com/example/gameboi/Leaderboard.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Leaderboard extends AppCompatActivity {
 
     LeaderBoardBE lb = new LeaderBoardBE();
@@ -11,6 +13,16 @@ public class Leaderboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+//        FileManager file = new FileManager();
+//        ArrayList<E> users = file.read();
+//
+//        ArrayList<E> topScorers = new ArrayList<>();
+//
+//        for (User item: users){
+//            if ()
+//        }
+
+
     }
 
 
