0cbf148f55e07675d889d60602c5264e879c6738
Anjali11 <anjali.parikh@mail.utoronto.ca>
2019-11-29 19:37:39 -0500

    added hard and easy calculation classes. Added abstract class. Merged round classes. Made leaderboard code better.

--- phase2/app/src/main/java/com/example/gameboi/RockPaperScissors/RPSRoundDisplay.java
+++ phase2/app/src/main/java/com/example/gameboi/RockPaperScissors/RPSRoundDisplay.java
@@ -1,36 +1,46 @@
 package com.example.gameboi.RockPaperScissors;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.gameboi.R;
 import com.example.gameboi.UserClasses.User;
 
-public class RpSWonRoundDisplay extends AppCompatActivity {
+import java.util.HashMap;
+
+public class RPSRoundDisplay extends AppCompatActivity {
 
     private User player;
+    String userChoiceDisplay;
+    String cChoiceDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rp_swon_round_display);
 
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String userChoiceDisplay = intent.getStringExtra("userchoice");
-        String cChoiceDisplay = intent.getStringExtra("computerchoice");
-        player = getIntent().getParcelableExtra("player");
-        //Setting up the background Colour
-        View colour = findViewById(R.id.textView4); //finds random view
+        setVars();
+        setBackgroundColour();
+        setImages();
+        setAnnouncement();
+    }
+
+    private void setBackgroundColour() {
+        View colour = findViewById(R.id.textView14); //finds random view
         View Root = colour.getRootView(); //finds the root view
         Root.setBackgroundColor(player.getBackgroundColor()); //set background color
+    }
 
+    private void setImages() {
         ImageView uChoice = findViewById(R.id.imageView4);
         ImageView panda = findViewById(R.id.imageView3);
+
 
         if (cChoiceDisplay.equals("Rock")) {
             panda.setImageResource(R.drawable.rock);
@@ -54,11 +64,36 @@ public class RpSWonRoundDisplay extends AppCompatActivity {
         }
     }
 
+    private void setAnnouncement() {
+        TextView announce = (TextView) findViewById(R.id.textView4);
+
+        HashMap hash = buildMap();
+        if (hash.get(userChoiceDisplay).equals(cChoiceDisplay)) {
+            announce.setText("You have won this round! Press Continue to play again.");
+        } else if (!userChoiceDisplay.equals(cChoiceDisplay)) {
+            announce.setText("You have lost this round! Press Continue to play again.");
+        } else {
+            announce.setText("No one won this round! Press Continue to play again.");
+        }
+    }
+
+    private void setVars() {
+        Intent intent = getIntent();
+        userChoiceDisplay = intent.getStringExtra("userchoice");
+        cChoiceDisplay = intent.getStringExtra("computerchoice");
+        player = getIntent().getParcelableExtra("player");
+    }
+
+    public HashMap buildMap() {
+        HashMap<String, String> winnersRpS = new HashMap<>();
+        winnersRpS.put("Rock", "Scissors");
+        winnersRpS.put("Scissors", "Paper");
+        winnersRpS.put("Paper", "Rock");
+        return winnersRpS;
+    }
+
     public void continueGame(View view) {
         Intent intent = new Intent(this, RockPaperScissors.class);
-        //EditText editText = (EditText) findViewById(R.id.editText); //look up the id for text user inputted
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message); //create key value pair
         intent.putExtra("player", player);
         startActivity(intent); //now intent has key value
     }
--- phase2/app/src/main/java/com/example/gameboi/RockPaperScissors/RockPaperScissors.java
+++ phase2/app/src/main/java/com/example/gameboi/RockPaperScissors/RockPaperScissors.java
@@ -7,140 +7,79 @@ import android.view.View;
 
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.gameboi.ScorePages.FlashLoss;
 import com.example.gameboi.ScorePages.FlashWin;
 import com.example.gameboi.R;
 import com.example.gameboi.UserClasses.User;
 
-import java.util.HashMap;
-import java.util.Random;
-
 public class RockPaperScissors extends AppCompatActivity {
 
     public static int winsRpS = 0;
-    public static int lossesRpS = 0;
-    String[] arr = new String[] {"Rock", "Paper", "Scissors"};
     private User player;
+    private RPSabstract rpsCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rock_paper_scissors);
 
-        player = getIntent().getParcelableExtra("player");
+        setTypeCalc();
+        setIcon();
+        setScore();
+        setBackground();
+    }
 
-        ImageView icon = findViewById(R.id.imageView);
-        int resID = getResources().getIdentifier(player.getIcon(),
-                "drawable", getPackageName()); // this line of code grabs the resID based on user name
-        icon.setImageResource(resID);
-
-        TextView scoreboardRpS = findViewById(R.id.textView12);
-        scoreboardRpS.setText(String.valueOf(winsRpS + player.getPoints()));
-
-        //Setting up the background Colour
+    private void setBackground() {
         View colour = findViewById(R.id.textView3); //finds random view
         View Root = colour.getRootView(); //finds the root view
         Root.setBackgroundColor(player.getBackgroundColor()); //set background color
     }
 
-    private HashMap buildMap() {
-        HashMap<String, String> winnersRpS = new HashMap<>();
-        winnersRpS.put("Rock", "Scissors");
-        winnersRpS.put("Scissors", "Paper");
-        winnersRpS.put("Paper", "Rock");
-        return winnersRpS;
+    private void setTypeCalc() {
+        player = getIntent().getParcelableExtra("player");
+        // add if statement depending on user difficulty attribute
+        rpsCalc = new RPSCalcEasy(player);
+    }
+    private void setScore() {
+        TextView scoreboardRpS = findViewById(R.id.textView12);
+        scoreboardRpS.setText(String.valueOf(winsRpS + player.getPoints()));
+    }
+    private void setIcon() {
+        ImageView icon = findViewById(R.id.imageView);
+        int resID = getResources().getIdentifier(player.getIcon(),
+                "drawable", getPackageName()); // this line of code grabs the resID based on user name
+        icon.setImageResource(resID);
     }
 
-    public void RpSGamePlayed(String playerValue) {
-        HashMap<String, String> winnersRpS = buildMap();
-        
-        Random rand = new Random();
-        String computerchoice = arr[rand.nextInt(arr.length)];
 
-        if (computerchoice == "Scissors" && playerValue == "Rock") {
-            player.foundHiddenfeature();
-            Toast toast = Toast.makeText(this, "Congrats! You unlocked the hidden " +
-                    "feature, you're multiplier doubles!", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-
-
-        if (winnersRpS.get(playerValue).equals(computerchoice)) {
-            //user wins, do add to wins
-            winsRpS += 1;
-            System.out.println("W:");
-            System.out.println(winsRpS);
-            checker("won", playerValue, computerchoice);
-        } else if (computerchoice.equals(playerValue)) {
-            //to take into account number of games played
-            checker("tie", playerValue, computerchoice);
-        } else  {
-            //comp wins, add to losses
-            lossesRpS += 1;
-            System.out.println("L:");
-            System.out.println(lossesRpS);
-            checker("lost", playerValue, computerchoice);
-        }
-
-
+    public void Rock(View view) {
+        toNext(rpsCalc.RpSGamePlayed("Rock"));
     }
-
-    public void Rock(View view) { RpSGamePlayed("Rock"); }
 
     public void Paper(View view) {
-        RpSGamePlayed("Paper");
+        toNext(rpsCalc.RpSGamePlayed("Paper"));
     }
 
-    public void Scissors(View view) { RpSGamePlayed("Scissors"); }
+    public void Scissors(View view) {
+        toNext(rpsCalc.RpSGamePlayed("Scissors"));
+    }
 
-    private void checker(String outcome, String userchoice, String compchoice) {
-        TextView scoreboardRpS = findViewById(R.id.textView12);
-        if (lossesRpS == 2) {
-            System.out.println("Losses");
-            if (player.getLives() == 1) {
-                // go to 'you lost the game' screen
-                Intent intent = new Intent(this, FlashLoss.class);
-                intent.putExtra("player", player);
-                player.setPoints(Integer.parseInt(scoreboardRpS.getText().toString()));
-                startActivity(intent);
-            } else {
-                // go to 'you won the game' screen
-                Intent intent = new Intent(this, FlashWin.class);
-                intent.putExtra("player", player);
-                player.setPoints(Integer.parseInt(scoreboardRpS.getText().toString()));
-                startActivity(intent);
-            }
-        } else if (winsRpS == 3) {
-            System.out.println("wins");
-                // go to you won the game screen
-                Intent intent = new Intent(this, FlashWin.class);
-                intent.putExtra("player", player);
-                player.setPoints(Integer.parseInt(scoreboardRpS.getText().toString()));
-                startActivity(intent);
-        } else {
-            if (outcome.equals("won")) {
-                Intent intent = new Intent(this, RpSWonRoundDisplay.class);
-                intent.putExtra("userchoice", userchoice);
-                intent.putExtra("computerchoice", compchoice);
-                intent.putExtra("player", player);
-                startActivity(intent);
-            } else if (outcome.equals("lost")) {
-                //go to you lost this round screen
-                Intent intent = new Intent(this, RpsLostRoundDisplay.class);
-                intent.putExtra("userchoice", userchoice);
-                intent.putExtra("computerchoice", compchoice);
-                intent.putExtra("player", player);
-                startActivity(intent);
-            } else {
-                // new intent that displays try again
-                Intent intent = new Intent(this, tryAgainDisplay.class);
-                intent.putExtra("userchoice", userchoice);
-                intent.putExtra("computerchoice", compchoice);
-                intent.putExtra("player", player);
-                startActivity(intent);
-            }
+    private void toNext(String[] arr) {
+        if (arr[2].equals("FlashLoss")) {
+            Intent intent = new Intent(this, FlashLoss.class);
+            intent.putExtra("player", player);
+            startActivity(intent);
+        } else if (arr[2].equals("FlashWin")) {
+            Intent intent = new Intent(this, FlashWin.class);
+            intent.putExtra("player", player);
+            startActivity(intent);
+        } else if (arr[2].equals("Round")) {
+            Intent intent = new Intent(this, RPSRoundDisplay.class);
+            intent.putExtra("userchoice", arr[0]);
+            intent.putExtra("computerchoice", arr[1]);
+            intent.putExtra("player", player);
+            startActivity(intent);
         }
     }
 }
